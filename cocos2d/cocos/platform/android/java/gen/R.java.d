b243cc0a2e067aec5c9e73275bbf342161ba5697
I.Kazunori <minaka1412@gmail.com>
2014-12-04 10:44:06 +0900

    リポジトリリセット

--- cocos2d/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/InoueKazunori/Documents/Programs/Projects/Ilohas/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
--- proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi/objs-debug/cocos2dx_static/base/ccFPSImages.o: \
-  /Users/InoueKazunori/Documents/Programs/Projects/Ilohas/proj.android/../cocos2d/cocos/./base/ccFPSImages.c \
-  /Users/InoueKazunori/Documents/Programs/Projects/Ilohas/proj.android/../cocos2d/cocos/./base/ccFPSImages.h
-
-/Users/InoueKazunori/Documents/Programs/Projects/Ilohas/proj.android/../cocos2d/cocos/./base/ccFPSImages.h:
