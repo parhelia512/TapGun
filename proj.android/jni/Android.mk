de166f0b4d5a3ba1737f61f7c801fd42be575286
I.Kazunori <minaka1412@gmail.com>
2015-01-12 10:27:58 +0900

    Androidでのテクスチャ貼り付けテスト成功

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,7 +12,13 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/C++/Scene/TestScene.cpp \
+				   ../../Classes/C++/System/Errorfunc.cpp \
+				   ../../Classes/C++/System/Script.cpp \
+				   ../../Classes/C++/System/Shader.cpp \
+				   ../../Classes/C++/System/Sound.cpp \
+				   ../../Classes/C++/System/Wrapper.cpp \
+
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
