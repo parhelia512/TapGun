17f98038d8c6c9f75255296c95f6612d178dbdd3
I.Kazunori <minaka1412@gmail.com>
2015-01-21 10:00:11 +0900

    test

--- cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\Programs\Repository\TapGun\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml : \
-E:\Programs\Repository\TapGun\cocos2d\cocos\platform\android\java\AndroidManifest.xml \
--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\Programs\Repository\TapGun\proj.android\bin\AndroidManifest.xml : \
-E:\Programs\Repository\TapGun\proj.android\AndroidManifest.xml \
